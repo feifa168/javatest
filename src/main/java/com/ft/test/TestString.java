2a12cd08a75dd50347f107abf39e531477ecb634
Riding a snail to travel <16897822+feifa168@users.noreply.github.com>
2020-10-24 01:05:07 +0800

    Update TestString.java
    test an error

--- src/main/java/com/ft/test/TestString.java
+++ src/main/java/com/ft/test/TestString.java
@@ -3,6 +3,7 @@ package com.ft.test;
 public class TestString {
     public static void test123() {
         System.out.println("test123");
+        thi
     }
 
     public static void main(String[] args) {
