26d69f68b9d235a99070ff8cf08ba45059d4bcc8
Riding a snail to travel <16897822+feifa168@users.noreply.github.com>
2020-10-24 00:57:47 +0800

    Update TestString.java
    add test method

--- src/main/java/com/ft/test/TestString.java
+++ src/main/java/com/ft/test/TestString.java
@@ -1,7 +1,12 @@
 package com.ft.test;
 
 public class TestString {
+    public static void test123() {
+        System.out.println("test123");
+    }
+
     public static void main(String[] args) {
         System.out.println("hello");
+        TestString.test123();
     }
 }
