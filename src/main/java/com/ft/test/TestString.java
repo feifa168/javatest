7c2af95d55bc62f4c42831c26ec1f95745193c0b
Riding a snail to travel <16897822+feifa168@users.noreply.github.com>
2020-12-19 00:38:26 +0800

    Update TestString.java
    grammatical mistake

--- src/main/java/com/ft/test/TestString.java
+++ src/main/java/com/ft/test/TestString.java
@@ -3,7 +3,6 @@ package com.ft.test;
 public class TestString {
     public static void test123() {
         System.out.println("test123");
-        thi
     }
 
     public static void main(String[] args) {
